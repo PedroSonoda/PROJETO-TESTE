6d4c15c58a085ba73e3055a400b2a37868cfc094
PedroSonoda <159277350+PedroSonoda@users.noreply.github.com>
2024-03-01 14:53:54 -0300

    primeiro commit

---
+++ Nome_e_idade.java
@@ -0,0 +1,57 @@
+<<<<<<< HEAD
+package primeiro_projeto;
+import java.util.Scanner;
+public class Nome_e_idade {
+
+	public static void main(String[] args) {
+		//Variáveis
+		String nome,idade,conj;
+		//Instaciar classe Scanner
+		Scanner ler = new Scanner (System.in);
+		
+		//Entrada de dados
+		System.out.println("Informe o seu nome:") ;
+		nome = ler.nextLine();
+		
+		System.out.println("Informe a sua idade") ;
+		idade = ler.next();
+		
+		//Processamento
+		
+		//Saída
+		System.out.println("Seu nome é " + nome + " e você tem " +idade + " anos ");
+
+		// TODO Auto-generated method stub
+
+	}
+
+}
+=======
+package primeiro_projeto;
+import java.util.Scanner;
+public class Nome_e_idade {
+
+	public static void main(String[] args) {
+		//Variáveis
+		String nome,idade,conj;
+		//Instaciar classe Scanner
+		Scanner ler = new Scanner (System.in);
+		
+		//Entrada de dados
+		System.out.println("Informe o seu nome:") ;
+		nome = ler.nextLine();
+		
+		System.out.println("Informe a sua idade") ;
+		idade = ler.next();
+		
+		//Processamento
+		
+		//Saída
+		System.out.println("Seu nome é " + nome + " e você tem " +idade + " anos ");
+
+		// TODO Auto-generated method stub
+
+	}
+
+}
+>>>>>>> 12cea9627c0ae8c4b4c7c56faa9409608f7da2d7
---
+++ Soma.java
@@ -0,0 +1,59 @@
+<<<<<<< HEAD
+package primeiro_projeto;
+import java.util.Scanner;
+public class Soma {
+
+	public static void main(String[] args) {
+		//Variáveis
+		double a,b,som;
+		//Instaciar classe Scanner
+		Scanner ler = new Scanner (System.in);
+		
+		//Entrada de dados 
+		System.out.println("Informe valor 1:");
+		a = ler.nextDouble();
+		
+		
+		System.out.println("Informe o valor 2:");
+		b = ler.nextDouble();
+		
+		//Processamento
+		som = a+b;
+		//Saída
+		System.out.println("O resultado é :" + som);
+		ler.close();
+		// TODO Auto-generated method stub
+
+	}
+
+}
+=======
+package primeiro_projeto;
+import java.util.Scanner;
+public class Soma {
+
+	public static void main(String[] args) {
+		//Variáveis
+		double a,b,som;
+		//Instaciar classe Scanner
+		Scanner ler = new Scanner (System.in);
+		
+		//Entrada de dados 
+		System.out.println("Informe valor 1:");
+		a = ler.nextDouble();
+		
+		
+		System.out.println("Informe o valor 2:");
+		b = ler.nextDouble();
+		
+		//Processamento
+		som = a+b;
+		//Saída
+		System.out.println("O resultado é :" + som);
+		ler.close();
+		// TODO Auto-generated method stub
+
+	}
+
+}
+>>>>>>> 12cea9627c0ae8c4b4c7c56faa9409608f7da2d7
